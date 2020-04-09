8555a1bf9ce2ce6af07b96dcdd4535eae850bc3a
Tijana Pandevska <pandevska@gmail.com>
2020-04-09 19:34:04 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -6,14 +6,66 @@ class Student {
 
 	//TODO constructor
 
+	public Student(String index, String firstName, String lastName, List<Integer> labPoints) {
+		this.index = index;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.labPoints = labPoints;
+	}
+
+
 	//TODO seters & getters
 
+	public String getIndex() {
+		return index;
+	}
+
+	public void setIndex(String index) {
+		this.index = index;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public List<Integer> getLabPoints() {
+		return labPoints;
+	}
+
+	public void setLabPoints(List<Integer> labPoints) {
+		this.labPoints = labPoints;
+	}
 
 	public double getAverage() {
-		//TODO
+		float avg = 0.0;
+		if(!this.labPoints.isEmpty()) {
+			for (float points : this.labPoints) {
+				avg += points;
+			}
+		}
+		avg = avg / labPoints.size;
+		return avg;
 	}
 
 	public boolean hasSignature() {
-		//TODO
+		if(this.labPoints.size > 8)
+			return true;
+		else
+			return false;
 	}
+
 }
+
+
